bd2b234c93429338711efb66858e71a09797c9da
eL1v-no <302854518@student.rocmondriaan.nl>
2025-01-13 12:20:38 +0100

    knoppen

--- js/main.js
+++ js/main.js
@@ -4,8 +4,26 @@ const planets = ["Mercurius", "Venus", "Aarde", "Mars", "Jupiter", "Saturnus", "
 const routes = ["Home", "Universum", "Planeten"];
 
 
+let kleurBtn = document.querySelector('.btn-make-light');
+let planeetBtn = document.querySelector('.btn-add-planet');
+let darkBtn = document.querySelector('.btn-make-dark');
 
 
+darkBtn.addEventListener('click', function(){
+    document.body.style.backgroundColor ='black';
+    document.body.style.color = 'white';
+});
+
+
+//maakt de witte thema knop
+kleurBtn.addEventListener('click', function(){
+    document.body.style.backgroundColor = 'white';
+    document.body.style.color = 'black';
+});
+
+
+//de hinderlijke planeey
+//planeetBtn.addEventListener('click', addPlanet);
 
 function addPlanet(){
     const planet = prompt("Welke planeet wil je toevoegen?");
